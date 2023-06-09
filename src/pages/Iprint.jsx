c3dc1e121bd2df07820b4bb70f4b7cbdd2a390dc
Anjum <88701810+Anjum7866@users.noreply.github.com>
2023-06-09 13:02:37 -0700

    image added

--- src/pages/Iprint.jsx
+++ src/pages/Iprint.jsx
@@ -8,7 +8,20 @@ const Iprint = () => {
       <Title title={'iPrint'}/>
 
       <div className="stats flex md:flex-row flex-col">
-          
+     
+              <img src="/automation.jpg"  className="w-1/2" alt="" />
+              <img src="/automation1.jpeg"  className="w-1/2 ml-4" alt="" />
+           
+      </div>
+      <div>
+      <img src="/hrms.png"  className="w-full mt-4 " alt="" />
+      </div>
+      <div>
+        <h2>Redivivus Technology Private Limited</h2>
+        <p> is a software product development and services company, serving its customer and clients from its global delivary centre in Pune,India.
+Redivius is focused to develop the various technical solutions for industrial automation, printing and inspection , image processing,data management.a software  product development and services company, serving its customer and clients from its global delivary centre in Pune,India.
+Redivius is focused to develop the various technical solutions for industrial automation, printing and inspection , image processing,data management.a software product development and services company, serving its customer and clients from its global delivary centre in Pune,India.
+Redivius is focused to develop the various technical solutions for industrial automation, printing and inspection , image processing,data management.a software </p>
       </div>
 
 
