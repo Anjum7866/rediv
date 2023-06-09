65d3d2228c69a102d2c702026b12ff92335e57f7
Anjum <88701810+Anjum7866@users.noreply.github.com>
2023-06-09 08:16:59 -0700

    Sidebar made

--- src/App.js
+++ src/App.js
@@ -1,42 +1,11 @@
-import { useEffect } from "react";
-import "./FrameComponent.css";
-import logo from './../public/images/logonew.png'
+import "./App.css";
+import logo from './images/logonew.png';
+import social from './images/social.png';
+
 const App = () => {
-  useEffect(() => {
-    const scrollAnimElements = document.querySelectorAll(
-      "[data-animate-on-scroll]"
-    );
-    const observer = new IntersectionObserver(
-      (entries) => {
-        for (const entry of entries) {
-          if (entry.isIntersecting || entry.intersectionRatio > 0) {
-            const targetElement = entry.target;
-            targetElement.classList.add("animate");
-            observer.unobserve(targetElement);
-          }
-        }
-      },
-      {
-        threshold: 0.15,
-      }
-    );
-
-    for (let i = 0; i < scrollAnimElements.length; i++) {
-      observer.observe(scrollAnimElements[i]);
-    }
-
-    return () => {
-      for (let i = 0; i < scrollAnimElements.length; i++) {
-        observer.unobserve(scrollAnimElements[i]);
-      }
-    };
-  }, []);
-
   return (
-    <div className="image-1-parent" data-animate-on-scroll>
-      <img className="image-1-icon" alt="" src="logo" />
-      <img className="image-2-icon" alt="" src="/image-2@2x.png" />
-      <div className="frame-child" />
+    <div className="desktop-1">
+      <div className="desktop-1-child" alt="" src="/rectangle-1.svg" ></div>
       <div className="about-us-sitemap-container">
         <p className="about-us">About Us</p>
         <p className="about-us">Sitemap</p>
@@ -59,15 +28,7 @@ const App = () => {
         <p className="about-us">Reosys Complete ERP</p>
         <p className="about-us">HRMS</p>
       </div>
-      <img className="frame-item" alt="" src="/rectangle-2.svg" />
-      <div className="frame-inner" />
-      <div className="contact-us">Contact-us</div>
-      <div className="rectangle-div" />
-      <div className="about-us1">About-us</div>
-      <div className="redivivus">
-        <p className="about-us">&nbsp;</p>
-        <p className="about-us">&nbsp;</p>
-        <p className="about-us">&nbsp;</p>
+      <div className="redivivus" style={{marginTop:10}}>
         <p className="about-us">Redivivus</p>
       </div>
       <div className="product">Product</div>
@@ -78,78 +39,10 @@ const App = () => {
         <p className="about-us">Consultancy Services</p>
         <p className="about-us">Industry Services</p>
       </div>
-      <div className="redivivus-technology-private-container">
-        <p className="about-us">
-          <b className="redivivus-technology-private">
-            Redivivus Technology Private Limited
-          </b>
-          <span>
-            {" "}
-            is a software product development and services company, serving its
-            customer and clients from its global delivary centre in Pune,India.
-          </span>
-        </p>
-        <p className="about-us">
-          Redivius is focused to develop the various technical solutions for
-          industrial automation, printing and inspection , image processing,data
-          management.a software product development and services company,
-          serving its customer and clients from its global delivary centre in
-          Pune,India.
-        </p>
-        <p className="about-us">
-          Redivius is focused to develop the various technical solutions for
-          industrial automation, printing and inspection , image processing,data
-          management.a software product development and services company,
-          serving its customer and clients from its global delivary centre in
-          Pune,India.
-        </p>
-        <p className="about-us">{`Redivius is focused to develop the various technical solutions for industrial automation, printing and inspection , image processing,data management.a software `}</p>
-      </div>
-      <img
-        className="fb-removebg-preview-1-icon"
-        alt=""
-        src="/fbremovebgpreview-1@2x.png"
-      />
-      <img className="logor-1-icon" alt="" src="/logor-1@2x.png" />
-      <b className="products">Products</b>
-      <div className="frame-child1" />
-      <div className="english">English</div>
-      <div className="already-a-customer-container">
-        <p className="about-us">
-          <span>
-            <span className="already-a-customer">
-              {" "}
-              Already a customer? Sign in
-            </span>
-          </span>
-        </p>
-        <p className="about-us">
-          <span>
-            <span className="already-a-customer">&nbsp;</span>
-          </span>
-        </p>
-        <p className="about-us">
-          <span>
-            <span>Condition of use privacy Notice Interest-Based ads</span>
-          </span>
-        </p>
-        <p className="about-us">
-          <span>
-            <span>{`                     `}</span>
-          </span>
-          <span>
-            <span className="redivivus-inc1"> 2022 Redivivus Inc</span>
-            <span className="span">.</span>
-          </span>
-        </p>
-      </div>
-      <img
-        className="hrms-1-icon"
-        alt=""
-        src="/hrms-1@2x.png"
-        data-animate-on-scroll
-      />
+      <img className="fb-removebg-preview-1-icon" src={social} alt="Social" />;
+      <img className="logor-1-icon" alt="logo" src={logo} />
       <b className="connect">Connect</b>
+     
     </div>
   );
 };
