d8fd46b2196aa9fd1324bf964ed307068be7941a
Aliyasim <134918942+Aliyasim@users.noreply.github.com>
2023-06-10 13:13:46 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,42 @@
+<<<<<<< Updated upstream
 import './App.css';
 import Layout from './pages/Layout';
+=======
+import { useEffect } from "react";
+import "./FrameComponent.css";
+import logo from './images/logonew.png';
+
+const App = () => {
+  useEffect(() => {
+    const scrollAnimElements = document.querySelectorAll(
+      "[data-animate-on-scroll]"
+    );
+    const observer = new IntersectionObserver(
+      (entries) => {
+        for (const entry of entries) {
+          if (entry.isIntersecting || entry.intersectionRatio > 0) {
+            const targetElement = entry.target;
+            targetElement.classList.add("animate");
+            observer.unobserve(targetElement);
+          }
+        }
+      },
+      {
+        threshold: 0.15,
+      }
+    );
+
+    for (let i = 0; i < scrollAnimElements.length; i++) {
+      observer.observe(scrollAnimElements[i]);
+    }
+
+    return () => {
+      for (let i = 0; i < scrollAnimElements.length; i++) {
+        observer.unobserve(scrollAnimElements[i]);
+      }
+    };
+  }, []);
+>>>>>>> Stashed changes
 
 function App() {
   return (
