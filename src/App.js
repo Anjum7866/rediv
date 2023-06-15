82b37668fddaf75e1fa730e23ae932594b7c55fc
Anjum <88701810+Anjum7866@users.noreply.github.com>
2023-06-15 10:42:40 -0700

    app working perfect

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import './App.css';
-import ImageSlider from './components/ImageSlider';
 import Layout from './pages/Layout';
 const App = () => {
   return (
     <div className="App">
-      {/* <Layout/> */}
-      <ImageSlider />
+      <Layout/>
 
     </div>
   );
